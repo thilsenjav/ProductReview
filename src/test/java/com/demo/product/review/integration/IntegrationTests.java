5afe1c4951716161e61cb35110b4938f94087c15
thilsenjav <thilsenjav@gmail.com>
2018-01-31 13:39:02 +0530

    aaa

--- src/test/java/com/demo/product/review/integration/IntegrationTests.java
+++ src/test/java/com/demo/product/review/integration/IntegrationTests.java
@@ -8,5 +8,5 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="classpath:com.demo.product.review.integration",tags= {"@registration"},plugin= {"pretty"})
 public class IntegrationTests {
-
+//Test commit
 }
